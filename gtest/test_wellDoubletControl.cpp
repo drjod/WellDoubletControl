36f8b7895b21d2bf9dc2fa512609cac96767646d
Jens-Olaf Delfs <jod@gpi.uni-kiel.de>
2018-05-30 17:54:21 +0200

    scheme B looks ready

--- gtest/test_wellDoubletControl.cpp
+++ gtest/test_wellDoubletControl.cpp
@@ -1,85 +1,180 @@
 #include "wellDoubletControl.cpp"
-#define TEMPERATURE_2 30
+
+// for T_1 it is initial temperature, T_2 is kept constant
+#define TEMPERATURE_1 60
+#define TEMPERATURE_2 10
 
 
 class Simulator
 {
 	double T1, T2;
-	WellDoubletControl* wellDoublet;
+	WellDoubletControl* wellDoubletControl;
+	bool flag_iterate;
 public:
 	Simulator() {}
-	Simulator(WellDoubletControl* _wellDoublet) : wellDoublet(_wellDoublet)
+	Simulator(WellDoubletControl* _wellDoubletControl) : wellDoubletControl(_wellDoubletControl)
 	{ /* set accuracy (epsilon) and fluid thermal capacity */ }
 
 	void set_temperature(const double& _T1, const double& _T2)
 	{ T1 = _T1; T2 = _T2; }
 
-	WellDoubletResult execute_timeStep(double Q_H, double value_target, 
+	bool get_flag_iterate()
+	{
+		return flag_iterate;
+	}
+
+	WellDoubletCalculation execute_timeStep(double Q_H, double value_target, 
 			double value_threshold, double T1_new, double T2_new)
 	{
-		wellDoublet->set_iterationValues(T1, T2);
-		wellDoublet->set_timeStepValues(Q_H,
+		wellDoubletControl->set_iterationValues(T1, T2);
+		wellDoubletControl->set_timeStepValues(Q_H,
 				value_target, value_threshold);
 
-		do  // iterate
+		//do  // iterate
 		{
-			wellDoublet->calculate_flowrate();
+			wellDoubletControl->calculate_flowrate();
 			set_temperature(T1_new, T2_new);			
-			wellDoublet->set_iterationValues(T1, T2);
+			wellDoubletControl->set_iterationValues(T1, T2);
 		}
-		while(wellDoublet->check_result());
+		//while(
+		flag_iterate = wellDoubletControl->check_result();
+		//);
 
-		return wellDoublet->get_result();
+		return wellDoubletControl->get_result();
 	}
 };
 
 
 class WellDoubletTest : public ::testing::TestWithParam<std::tr1::tuple<
-	char, double, double, double, double, double, 
-	double, double, bool> > {};
+	char, double, double, double, double, double, double, bool, bool> > {};
 
 
 TEST_P(WellDoubletTest, WellDoubletControl)
 {
+
 	Simulator simulator = Simulator(
 		WellDoubletControl::createWellDoubletControl(
 				std::tr1::get<0>(GetParam())));
 	
-	simulator.set_temperature(std::tr1::get<4>(GetParam()),  // T1_init
+	simulator.set_temperature(TEMPERATURE_1,  // T1_init
 				TEMPERATURE_2);  // T2_init
 
-	WellDoubletResult result = simulator.execute_timeStep(
+	WellDoubletCalculation result = simulator.execute_timeStep(
 		std::tr1::get<1>(GetParam()),  // Q_H
 		std::tr1::get<2>(GetParam()),  // value_target
 		std::tr1::get<3>(GetParam()),  // value_threshold 
-		std::tr1::get<5>(GetParam()),  // T1_new
+		std::tr1::get<4>(GetParam()),  // T1_new
 		TEMPERATURE_2);  // T2_new
 
-	EXPECT_EQ(std::tr1::get<6>(GetParam()), result.Q_H);
-	EXPECT_EQ(std::tr1::get<7>(GetParam()), result.Q_w);
-	EXPECT_EQ(std::tr1::get<8>(GetParam()), result.flag_powerrateAdapted);
+	EXPECT_NEAR(std::tr1::get<5>(GetParam()), result.Q_H, 1.e-3 * fabs(result.Q_H));
+	EXPECT_NEAR(std::tr1::get<6>(GetParam()), result.Q_w, 1.e-3 * fabs(result.Q_w));
+	EXPECT_EQ(std::tr1::get<7>(GetParam()), result.flag_powerrateAdapted);
+	EXPECT_EQ(std::tr1::get<8>(GetParam()), simulator.get_flag_iterate());
+
+//std::cout << result.Q_H << std::endl;
+//EXPECT_TRUE(true);
 }
 
 
-INSTANTIATE_TEST_CASE_P(TestWithParamsC1, WellDoubletTest, testing::Values(
+INSTANTIATE_TEST_CASE_P(SCHEMES, WellDoubletTest, testing::Values(
 	// scheme, Q_H,
 	// value_target, value_threshold,
-	// T1_init, T1_new
-	// result: Q_H, Q_w, flag_powerrateAdapted
+	// T1_new (given by Simulator, initially TEMPERATURE_1)
+	// Q_H, Q_w, flag_powerrateAdapted (results), flag_iterate
 
-	// scheme B - storing
+//	std::make_tuple('A', 1e6)
+	// Scheme A - storing
+	std::make_tuple('A', 1.e6,
+			70., 0.01, // T1_target, Q_w_max
+			70., // at target
+			1.e6, 0.00333333, false, false),  // does not reach threshold
+	std::make_tuple('A', 1e6,
+			70, 0.01, // T1_target, Q_w_max
+			90,  // beyond target
+			6.666667e5, 0.00333333, true, true),  // does not reach threshold
+	std::make_tuple('A', 1e6,
+			70, 0.01, // T1_target, Q_w_max
+			60,  // does not reach target
+			1.e6, 0.00333333, false, true),  // does not reach threshold
+//	std::make_tuple('A', 1e6,
+//			70, 0.01, // T1_target, Q_w_max
+//			90,  // passed threshold
+//			1333333.33333333, 0.00333333, true),
+//	std::make_tuple('A', 1e6,
+//			70, 0.01, // T1_target, Q_w_max
+//			70,  // exactly as threshold
+//			1e6, 0.00333333, false),
+//	// Scheme A - extracting
+//	std::make_tuple('A', -1e6,
+//			30, -0.01, // T1_target, Q_w_min
+//			40,  // does not reach threshold
+//			-1e6, -0.01, false),
+//	std::make_tuple('A', -1e6,
+//			30, -0.01, // T1_target, Q_w_min
+//			20,  // passed threshold
+//			-5e5, -0.01, true),
+//	std::make_tuple('A', -1e6,
+//			30, -0.01, // T1_target, Q_w_min
+//			30,  // exactly as threshold
+//			-1e6, -0.01, false),
+//
+//	// Scheme B - storing
 	std::make_tuple('B', 1e6,
-			0.01, 70, // Q_w_target, T_1_max
-			60, 60,  // does not reach threshold
-			1e6, 0.01, false),
+			0.01, 70., // Q_w_target, T_1_max
+			60.,  // does not reach threshold
+			1e6, 0.01, false, false),
 	std::make_tuple('B', 1e6,
-			0.001, 70, // Q_w_target, T_1_max
-			60, 80,  // passed threshold
-			2e5, 0.001, true),
+			0.001, 70., // Q_w_target, T_1_max
+			80.,  // passed threshold
+			3e5, 0.001, true, true),
 	std::make_tuple('B', 1e6,
-			0.001, 70, // Q_w_target, T_1_max
-			60, 70,  // exactly as threshold
-			1e6, 0.001, false)
+			0.001, 70., // Q_w_target, T_1_max
+			70.,  // exactly as threshold
+			1e6, 0.001, false, false),
+	// Scheme B - extracting
+	std::make_tuple('B', -1e6,
+			-0.01, 30., // Q_w_target, T_1_min
+			60.,  // does not reach threshold
+			-1e6, -0.01, false, false),
+	std::make_tuple('B', -1e6,
+			-0.001, 30., // Q_w_target, T_1_min
+			20.,  // passed threshold
+			-1e5, -0.001, true, true),
+	std::make_tuple('B', -1e6,
+			-0.001, 30., // Q_w_target, T_1_min
+			30.,  // exactly as threshold
+			-1e6, -0.001, false, false),
+	std::make_tuple('B', 1e6,
+			0.01, 70., // Q_w_target, T_1_max
+			60.,  // does not reach threshold
+			1e6, 0.01, false, false),
+
+//	// Scheme C - storing
+	std::make_tuple('C', 1e6,
+			50., 70., // DT_target, T_1_max
+			60.,  // does not reach threshold
+			1e6, 0.004, false, false),
+	std::make_tuple('C', 1e6,
+			50, 70, // DT_target, T_1_max
+			80,  // passed threshold
+			1.2e6, 0.004, true, true), 
+
+	std::make_tuple('C', 1e6,
+			50, 70, // DT_target, T_1_max
+			70,  // exactly as threshold
+			1e6, 0.004, false, false)
+//	//// Scheme C - extracting				value of T_2 ?????
+//	//std::make_tuple('C', -1e6,
+//	//		50, 30, // DT_target, T_1_min
+//	//		60,  // does not reach threshold
+//	//		-1e6, -0.01, false),
+//	//std::make_tuple('C', -1e6,
+//	//		50, 30, // DT_target, T_1_min
+//	//		20,  // passed threshold
+//	//		-1e5, -0.001, true),
+//	//std::make_tuple('C', -1e6,
+//	//		50, 30, // DT_target, T_1_min
+//	//		30,  // exactly as threshold
+//	//		-1e6, -0.001, false)
 ));
-
 
--- wellDoubletControl.cpp
+++ wellDoubletControl.cpp
@@ -1,25 +1,36 @@
 #ifndef WELLDOUBLETCONTROL_CPP
 #define WELLDOUBLETCONTROL_CPP
 
-#define Log(x) std::cout << x << std::endl 
+#define LOG(x) std::cout << x << std::endl 
 #include <math.h>
 #include <iostream>
 #include "comparison.h"
 
-struct WellDoubletResult
+class WellSchemeA;
+class WellSchemeB;
+class WellSchemeC;
+
+struct WellDoubletCalculation
 {
 	double Q_H, Q_w;  // power rate, flow rate
 	double T1, T2;  // temperature at well 1 ("warm well") and 2 ("cold well")
 	
 	bool flag_powerrateAdapted;
+
+	void adapt_powerrate(WellSchemeA* scheme);
+	void adapt_powerrate(WellSchemeB* scheme);
+	void adapt_powerrate(WellSchemeC* scheme);
+
+	void calculate_flowrate(WellSchemeA* scheme);
+	void calculate_flowrate(WellSchemeB* scheme);
+	void calculate_flowrate(WellSchemeC* scheme);
 };
 
 
-class WellDoubletControl
+struct WellDoubletControl
 {
-protected:
 	double epsilon;  // accuracy
-	WellDoubletResult result;
+	WellDoubletCalculation calculation;
 	double value_target, value_threshold;
 	// A: T1_target, Q_w_max 
 	// B: Q_w_target, T1_max 
@@ -36,26 +47,26 @@ public:
 	void set_timeStepValues(const double& _Q_H,
 		const double& _value_target, const double& _value_threshold) 
 	{ 
-		Log("set time step values");
-		result.Q_H = _Q_H;
+		LOG("set time step values");
+		calculation.Q_H = _Q_H;
 		value_target = _value_target;
 		value_threshold = _value_threshold;
 
-		result.flag_powerrateAdapted = false;
+		calculation.flag_powerrateAdapted = false;
 		initialize();
 	}
 
 	void set_iterationValues(const double& _T1, const double& _T2)  
 	{
-		Log("set iteration values");
-		result.T1 = _T1;
-		result.T2 = _T2;
+		LOG("set iteration values");
+		calculation.T1 = _T1;
+		calculation.T2 = _T2;
 	}
 
 	virtual void calculate_flowrate() = 0;
-	virtual void adapt_powerrate() = 0;
+
 	virtual bool check_result() = 0;	
-	WellDoubletResult& get_result()	{ return result; }
+	WellDoubletCalculation& get_result()	{ return calculation; }
 
 	static WellDoubletControl* createWellDoubletControl(
 						const char& selection);
@@ -65,27 +76,12 @@ public:
 class WellSchemeA : public WellDoubletControl
 {
 public:
-	void calculate_flowrate()
-	{
-		if(result.Q_H > 0)
-			result.Q_w = fmin(result.Q_H / (capacity  * (result.T1 - result.T2)), value_threshold);
-		else
-			result.Q_w = fmax(result.Q_H / (capacity  * (result.T1 - result.T2)), value_threshold);
-	}
-	
-	void adapt_powerrate()
-	{
-		Log("adapt power rate");
-		result.Q_H = result.Q_w *  capacity * (result.T1 - result.T2);
-		result.flag_powerrateAdapted = true;
-	}
-	
 	void initialize()
 	{
-		Log("initialize");
-		result.T1 = value_target;
+		LOG("initialize");
+		//calculation.T1 = value_target;
 
-		if(result.Q_H > 0.)
+		if(calculation.Q_H > 0.)
 		{
 			beyondThreshold = Comparison(new Greater(1.e-5));
 			check_for_flow_rate_adaption =  Comparison(new Smaller(1.e-5));
@@ -96,17 +92,26 @@ public:
 			check_for_flow_rate_adaption = Comparison(new Greater(1.e-5));
 		}
 	}
+
+	void calculate_flowrate() { calculation.calculate_flowrate(this); }
+
 	bool check_result()
 	{
-		Log("check result");
-		if(beyondThreshold(result.T1, value_target))
-			adapt_powerrate();
+		LOG("check result");
+		if(beyondThreshold(calculation.T1, value_target))
+		{
+			calculation.adapt_powerrate(this);
+			return true;
+		}
 		else
 		{
-			if(check_for_flow_rate_adaption(result.T1, value_target))
-				return true;  // iterate
+			if(check_for_flow_rate_adaption(calculation.T1, value_target))
+			{
+				calculation.calculate_flowrate(this);
+				return true;
+			}
 		}
-		return true;
+		return false;
 	}
 };
 
@@ -114,26 +119,23 @@ class WellSchemeB : public WellDoubletControl
 {
 
 public:
-	void calculate_flowrate() { result.Q_w = value_target; }
-	
-	void adapt_powerrate()
-	{
-		result.Q_H = result.Q_w *  capacity * (value_threshold - result.T2);
-		result.flag_powerrateAdapted = true;
-	}
-
 	void initialize()
 	{
-		if(result.Q_H > 0.)
+		if(calculation.Q_H > 0.)  // storing
 			beyondThreshold = Comparison(new Greater(0.));
-		else
+		else  // extracting
 			beyondThreshold = Comparison(new Smaller(0.));
 	}
 
+	void calculate_flowrate() { calculation.calculate_flowrate(this); }
+
 	bool check_result()
 	{
-		if(beyondThreshold(result.T1, value_threshold))
-			adapt_powerrate();
+		if(beyondThreshold(calculation.T1, value_threshold))
+		{
+			calculation.adapt_powerrate(this);
+			return true;
+		}
 		return false;
 	}
 };
@@ -143,20 +145,9 @@ class WellSchemeC : public WellDoubletControl
 {
 
 public:
-	void calculate_flowrate()
-	{
-		result.Q_w = result.Q_H / (capacity *  value_target);
-	}
-	
-	void adapt_powerrate()
-	{
-		result.Q_H = result.Q_w * capacity * (value_threshold - result.T2);
-		result.flag_powerrateAdapted = true;
-	}
-
 	void initialize()
 	{
-		if(result.Q_H > 0.)
+		if(calculation.Q_H > 0.)
 		{
 			beyondThreshold = Comparison(new Greater(0.));
 		}
@@ -166,11 +157,13 @@ public:
 		}
 	}
 
+	void calculate_flowrate() { calculation.calculate_flowrate(this); }
+
 	bool check_result()
 	{
-		if(beyondThreshold(result.T1, value_threshold))
+		if(beyondThreshold(calculation.T1, value_threshold))
 		{
-			adapt_powerrate();
+			calculation.adapt_powerrate(this);
 			return true;
 		}
 		return false;
@@ -196,6 +189,53 @@ WellDoubletControl* WellDoubletControl::createWellDoubletControl(
 			break;
 	}
 	return 0;
+}
+
+
+void WellDoubletCalculation::adapt_powerrate(WellSchemeA* scheme)
+{
+	LOG("adapt power rate");
+	Q_H = Q_w * scheme->capacity * (2 * scheme->value_target - T1 - T2);
+	flag_powerrateAdapted = true;
+
+}
+
+void WellDoubletCalculation::adapt_powerrate(WellSchemeB* scheme)
+{
+	LOG("adapt power rate");
+	Q_H = Q_w * scheme->capacity * (scheme->value_threshold - T2);
+	flag_powerrateAdapted = true;
+}
+
+void WellDoubletCalculation::adapt_powerrate(WellSchemeC* scheme)
+{
+	LOG("adapt power rate");
+	Q_H = Q_w * scheme->capacity * (scheme->value_threshold - T2);
+	flag_powerrateAdapted = true;
+
+}
+
+
+void WellDoubletCalculation::calculate_flowrate(WellSchemeA* scheme)
+{
+	if(Q_H > 0)
+		Q_w = fmin(Q_H / (scheme->capacity  * (scheme->value_target - T2)), scheme->value_threshold);
+	else
+		Q_w = fmax(Q_H / (scheme->capacity  * (scheme->value_target - T2)), scheme->value_threshold);
+}
+
+void WellDoubletCalculation::calculate_flowrate(WellSchemeB* scheme)
+{
+	Q_w = scheme->value_target;
+}
+
+void WellDoubletCalculation::calculate_flowrate(WellSchemeC* scheme)
+{
+	// no range check
+	if(Q_H > 0)
+		Q_w = Q_H / (scheme->capacity *  scheme->value_target);
+	else
+		Q_w = Q_H / (scheme->capacity *  scheme->value_target);
 }
 
 #endif
