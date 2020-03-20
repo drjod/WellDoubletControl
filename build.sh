e506e7f3675118fc96db3b1db1b5b57304db286c
drjod <jens.delfs@ifg.uni-kiel.de>
2020-03-20 23:08:44 +0100

    adapt build file for cluster

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+module load cmake/3.15.4
+module load intel/18.0.4
 
 mkdir -p build
 cd build
-cmake ..
+cmake .. -DCMAKE_CXX_COMPILER=icpc
 make
+
+
+#  mkdir -p build
+#  cd build
+#  cmake ..
+#  make
 
