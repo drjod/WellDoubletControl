f5cd44706eb2fe1482da5ac3b8ac4dc1e8a96755
drjod <jens.delfs@ifg.uni-kiel.de>
2023-02-21 14:52:00 +0100

    cleared typing error

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-module load cmake/3.15.4
-module load intel/18.0.4
+# module load cmake/3.15.4
+# module load intel/18.0.4
+# 
+# mkdir -p build
+# cd build
+# cmake .. -DCMAKE_CXX_COMPILER=icpc
+# make
+
 
 mkdir -p build
 cd build
-cmake .. -DCMAKE_CXX_COMPILER=icpc
+cmake ..
 make
-
-
-#  mkdir -p build
-#  cd build
-#  cmake ..
-#  make
 
