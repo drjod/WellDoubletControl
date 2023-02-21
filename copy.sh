8422f6a0b1c5de24b6fbda67d78a3dd71924bad1
drjod <jens.delfs@ifg.uni-kiel.de>
2023-02-21 14:51:35 +0100

    build.sh

--- copy.sh
+++ copy.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-simulator_file=../ogs_kb1
-wdc_folder=$simulator_file/Libs/WellDoubletControl
+simulator_folder=../testingEnvironment/rzcluster/ogs/ogs_kb1
+wdc_folder=$simulator_folder/Libs/WellDoubletControl
 
 mkdir -p $wdc_folder
 
 cp build/src/libwellDoubletControl.a $wdc_folder
-cp src/wellDoubletControl.cpp $wdc_folder
-cp src/wellDoubletControl.h $wdc_folder
-cp src/comparison.h $wdc_folder
-cp build/wdc_config.h $wdc_folder  
-
+cp src/*.h $wdc_folder
+cp build/wdc_config.h $wdc_folder
